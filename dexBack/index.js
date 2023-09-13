9f25b00250d0ecac88b8d0d808d447b3ea04876c
Saman <samangithoseinpour@gmail.com>
2023-09-13 12:29:24 +0330

    feat: get token prices from moralis api

--- dexBack/index.js
+++ dexBack/index.js
@@ -1,15 +1,34 @@
-const express = require("express");
-const Moralis = require("moralis").default;
+const express = require('express');
+const Moralis = require('moralis').default;
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
+const cors = require('cors');
+require('dotenv').config();
 const port = 3001;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/tokenPrice", async (req, res) => {
-  return res.status(200).json(usdPrices);
+app.get('/tokenPrice', async (req, res) => {
+  const { query } = req;
+
+  console.log(query);
+
+  const reponseOne = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressOne,
+  });
+
+  const reponseTwo = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressTwo,
+  });
+
+  const usdPrice = {
+    tokenOne: reponseOne.raw.usdPrice,
+    tokenTwo: reponseTwo.raw.usdPrice,
+
+    ratio: reponseOne.raw.usdPrice / reponseTwo.raw.usdPrice,
+  };
+
+  return res.status(200).json(usdPrice);
 });
 
 Moralis.start({

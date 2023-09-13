789fc7620c4a843ff4606515160fbff96bd92c56
Saman <samangithoseinpour@gmail.com>
2023-09-13 11:46:58 +0330

    feat: select token

--- dex/src/components/SwapInputs.jsx
+++ dex/src/components/SwapInputs.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { Input } from 'antd';
+import { Input, Modal } from 'antd';
 import tokenList from '../tokenList.json';
 import { DownOutlined, ArrowDownOutlined } from '@ant-design/icons';
 
@@ -10,6 +10,9 @@ const SwapInputs = () => {
 
   const [tokenOne, setTokenOne] = useState(tokenList[0]);
   const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeToken, setChangeToken] = useState(1);
 
   const [prices, setPrices] = useState(null);
 
@@ -25,8 +28,45 @@ const SwapInputs = () => {
     setTokenTwo(one);
   };
 
+  const openModal = (asset) => {
+    setChangeToken(asset);
+    setIsOpen(true);
+  };
+
+  const modifyToken = (i) => {
+    changeToken === 1 ? setTokenOne(tokenList[i]) : setTokenTwo(tokenList[i]);
+
+    setIsOpen(false);
+  };
+
   return (
     <>
+      <Modal
+        title="Select a Token"
+        open={isOpen}
+        footer={null}
+        onCancel={() => setIsOpen(false)}
+      >
+        <div className="modalContent">
+          {tokenList &&
+            tokenList.map((e, i) => {
+              return (
+                <div
+                  className="tokenChoice"
+                  key={i}
+                  onClick={() => modifyToken(i)}
+                >
+                  <img src={e.img} alt={e.ticker} className="tokenLogo" />
+                  <div className="tokenChoiceNames">
+                    <div className="tokenName">{e.name}</div>
+                    <div className="tokenTicker">{e.ticker}</div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </Modal>
+
       <Input
         placeholder="0"
         value={tokenOneAmount}
@@ -37,12 +77,12 @@ const SwapInputs = () => {
       <div className="switchButton" onClick={switchTokens}>
         <ArrowDownOutlined className="switchArrow" />
       </div>
-      <div className="assetOne">
+      <div className="assetOne" onClick={() => openModal(1)}>
         <img src={tokenOne.img} alt="assets one logo" className="assetLogo" />
         {tokenOne.ticker}
         <DownOutlined />
       </div>
-      <div className="assetTwo">
+      <div className="assetTwo" onClick={() => openModal(2)}>
         <img src={tokenTwo.img} alt="assets two logo" className="assetLogo" />
         {tokenTwo.ticker}
         <DownOutlined />

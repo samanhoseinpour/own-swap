4a2c7d1b3ab5bd59267f470d6b7cd6ba893f74d4
Saman <samangithoseinpour@gmail.com>
2023-09-24 11:47:09 +0330

    feat: connect to metamask

--- dex/src/App.jsx
+++ dex/src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useConnect, useAccount } from 'wagmi';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 
 import Header from './components/Header';
 import { Swap, Tokens } from './components';
 
 const App = () => {
+  const { address, isConnected } = useAccount();
+  const { connect } = useConnect({
+    connector: new MetaMaskConnector(),
+  });
+
   return (
     <div className="App">
-      <Header />
+      <Header address={address} isConnected={isConnected} connect={connect} />
       <div className="mainWindow">
         <Routes>
-          <Route path="/" element={<Swap />}></Route>
+          <Route
+            path="/"
+            element={<Swap isConnected={isConnected} address={address} />}
+          ></Route>
           <Route path="/tokens" element={<Tokens />}></Route>
         </Routes>
       </div>
